a256a797e93901dddd5a36328080cc14fc948fc5
HostilePlayer <mart5e86@stud.kea.dk>
2022-09-26 13:21:34 +0200

    lavede commentar

--- src/Lamp.java
+++ src/Lamp.java
@@ -2,37 +2,41 @@ public class Lamp {
     private String name;
     private boolean tændt;
 
+    //lave metoden det skal lave elementer
     public Lamp(String name, boolean tændt){
         this.name = name;
         this.tændt = tændt;
     }
+
+    //laver Lamp som element
     public Lamp(String name){
         this.tændt = false;
         this.name = name;
     }
-
+    //laver turn on
     public void turnOn(){
         tændt =true;
     }
+    //laver turn off
     public void turnOff(){
         tændt =false;
     }
-
-
+    //setter bolian værdig
     public void setTaendt(boolean tilstand) {
         tændt = tilstand;
     }
-
+    // ændre Lamp bolean til det modsatte
     public void toggle(){
         if (!tændt)
             tændt=true;
         else
             tændt=false;
     }
+    //returner vis
     public boolean isOn(){
         return tændt;
     }
-
+    // laver bolean værdig til string
     public String toString(){
         String text;
         if (tændt == true)
--- src/LampToggle.java
+++ src/LampToggle.java
@@ -2,6 +2,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class LampToggle {
+    //laver 5 Lamp elemtner
     public static void main(String[] args) {
         Lamp lampe1 = new Lamp("lampe1");
         Lamp lampe2 = new Lamp("lampe2");
@@ -9,26 +10,29 @@ public class LampToggle {
         Lamp lampe4 = new Lamp("lampe4");
         Lamp lampe5 = new Lamp("lampe5");
 
+        //sitter brugers valg til null
         Lamp selectedLamp = null;
-
+        //laver scanner
         Scanner sc = new Scanner(System.in);
-
+        //laver int valg
         int valg;
+        //starter loop
         do {
+            //printer Lamp state og navn
             System.out.println(lampe1);
             System.out.println(lampe2);
             System.out.println(lampe3);
             System.out.println(lampe4);
             System.out.println(lampe5);
-
+            //giver brugeret et valg
             System.out.println("Hvilken lampe skal ændres? (0 er afslut)");
-
+            //chekker om brugeren giver et tal
             try {
                 valg = sc.nextInt();
             } catch (InputMismatchException e) {
                 valg = 0;
             }
-
+            //brugeren vælger lamp
             if (valg > 0 && valg <= 5) {
                 if (valg == 1)
                     selectedLamp = lampe1;
@@ -40,7 +44,7 @@ public class LampToggle {
                     selectedLamp = lampe4;
                 else if (valg == 5)
                     selectedLamp = lampe5;
-
+                //tager brugerns valg og putter den til medtoden toggle
                 selectedLamp.toggle();
             }
         } while (valg != 0);
